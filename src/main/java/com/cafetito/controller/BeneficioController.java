f353a1a064918ae2702d0e512dda9fe0ee68111e
Andergit164 <anderson.quiroa164@gmail.com>
2023-05-05 22:08:53 -0600

    se agrego @CrossOrigin

--- src/main/java/com/cafetito/controller/BeneficioController.java
+++ src/main/java/com/cafetito/controller/BeneficioController.java
@@ -25,6 +25,7 @@ import javax.ws.rs.Produces;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -87,6 +88,7 @@ public class BeneficioController {
      * @url rutas para la entidad agricultor
      */
     
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/farmer/listar", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<AgricultorEntity>> listarAgricultores() {
